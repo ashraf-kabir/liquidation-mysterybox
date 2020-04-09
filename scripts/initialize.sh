ec117a606bc9ea7f2f7375e288a7de32f1cef1df
ryan wong <wongryan2001@gmail.com>
2020-04-09 16:33:32 -0400

    add base code

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,1 +1,7 @@
 # This script will copy all files into release to build project based on config
+cp -R mkdcore/initialize/* release;
+composer install;
+# /usr/bin/php composer.phar install;
+cp -R vendor release;
+cp -R composer.json release;
+cp -R composer.lock release;
--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -1,4 +1,4 @@
-cd src;
+cd release;
 /usr/bin/php vendor/bin/phinx rollback -e development -t 0
 /usr/bin/php vendor/bin/phinx migrate -e development
 /usr/bin/php vendor/bin/phinx seed:run -e development
