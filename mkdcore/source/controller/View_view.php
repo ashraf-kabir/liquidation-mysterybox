b02b1e1642be46212743372a9f5e247785bfa34e
Tatenda <tatemunenge@gmail.com>
2020-07-06 22:03:51 +0200

    view page

--- mkdcore/source/controller/View_view.php
+++ mkdcore/source/controller/View_view.php
@@ -5,29 +5,30 @@ if ($layout_clean_mode) {
 	echo '<style>#content{padding:0px !important;}</style>';
 }
 ?>
-<div class="row">
-	<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-		<div class="page-header" id="top">
-			<h2 class="pageheader-title"><?php echo $view_model->get_heading();?> </h2>
-			<div class="page-breadcrumb">
-				<nav aria-label="breadcrumb">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item"><a href="/{{{portal}}}/dashboard" class="breadcrumb-link">xyzDashboard</a></li>
-						<li class="breadcrumb-item"><a href="/{{{portal}}}{{{route}}}" class="breadcrumb-link"><?php echo $view_model->get_heading();?></a></li>
-						<li class="breadcrumb-item active" aria-current="page">xyzView</li>
-					</ol>
-				</nav>
-			</div>
-		</div>
-	</div>
+<div class="tab-content" id="nav-tabContent">
+              <!-- Bread Crumb -->
+<div aria-label="breadcrumb">
+    <ol class="breadcrumb pl-0 mb-4 bg-background d-flex justify-content-center justify-content-md-start">
+        <li class="breadcrumb-item active" aria-current="page">
+            <a href="/{{{portal}}}/dashboard" class="breadcrumb-link">xyzDashboard</a>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+            <a href="/{{{portal}}}{{{route}}}" class="breadcrumb-link"><?php echo $view_model->get_heading();?></a>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+            xyzView
+        </li>
+    </ol>
 </div>
+<h1 class="primaryHeading mb-4 text-center text-md-left">
+    xyzView <?php echo $view_model->get_heading();?>
+</h1>
 <div class="row">
     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
         <div class="card">
-            <h5 class="card-header"><?php echo $view_model->get_heading();?> xyzDetails</h5>
-                <div class="card-body">
-{{{input}}}{{{custom_view_view}}}
-                </div>
+            <div class="card-body">
+                {{{input}}}{{{custom_view_view}}}
+            </div>
         </div>
     </div>
 </div>
