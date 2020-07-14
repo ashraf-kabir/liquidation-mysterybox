4a3dafae08b5faac1573a5c274efabec4f071217
Tatenda <tatemunenge@gmail.com>
2020-07-14 18:08:23 +0200

    finish subscription middleware

--- mkdcore/source/middleware/Subscription_middleware.php
+++ mkdcore/source/middleware/Subscription_middleware.php
@@ -24,15 +24,38 @@ class Subscription_middleware
     public function run()
     {
         $session = $this->_controller->get_session();
-        die('middleware running');
-
+        $this->_controller->load->model('stripe_subscriptions_model');
+    
         if(!empty($session))
         {
             $user_id = $session['user_id'];
             $role_id = $session['role'];
+            $user_sub = $this->_controller->stripe_subscriptions_model->get_by_fields([
+                'user_id' => $user_id,
+                'role_id' => $role_id
+            ]);
+            
+            $portal = $this->_controller->uri->segment(2);
+
+            if(empty($user_sub))
+            {
+                return $this->_controller->redirect("/{$portal}/stripe_subscriptions", 'refresh');
+            }
+
+            /**
+             * status 5 equal canceled
+             * @see subscription_model mapping
+             */
+            if($user_sub->status == 5)
+            {
+                return $this->_controller->redirect("/{$portal}/stripe_subscriptions", 'refresh');
+            }
+
+            return FALSE;
+    
         }
 
-        return FALSE;
+        return $this->_controller->redirect("/{$portal}/stripe_subscriptions", 'refresh');
     }
 
 }
