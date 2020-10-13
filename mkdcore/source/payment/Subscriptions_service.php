9608a57d5d113e9a18f64ea415b3b803f71e5724
Tatenda <tatemunenge@gmail.com>
2020-10-13 21:07:06 +0200

    add subscriptions service class

---
+++ mkdcore/source/payment/Subscriptions_service.php
@@ -0,0 +1,14 @@
+<?php if (! defined('BASEPATH')) exit('No direct script access allowed');
+/*Powered By: Manaknightdigital Inc. https://manaknightdigital.com/ Year: 2019*/
+/**
+ * Payments Service
+ *
+ * @copyright 2019 Manaknightdigital Inc.
+ * @link https://manaknightdigital.com
+ * @license Proprietary Software licensing
+ */
+
+class Subscriptions_service{
+  
+    
+}
