c7cc4f17dffd2c4e80768e1d40501de4fe84214c
Muhammad <zeeshan72awan@gmail.com>
2020-11-19 22:46:00 +0500

    Ecom

--- scripts/copyToProd.dist.sh
+++ scripts/copyToProd.dist.sh
@@ -1,7 +1,8 @@
 # This script will copy release to prod. Copy this script don't use this one
 cd ..;
-cp -r ./release/* /var/www/devbluegable.manaknightdigital.com/htdocs;
-cd /var/www/devbluegable.manaknightdigital.com/htdocs;
+cp -r ./release/* /var/www/devecom.vegasliquidationstore.com/htdocs;
+cd /var/www/devecom.vegasliquidationstore.com/htdocs;
 chown -R www-data:www-data *;
 chmod -R 755 *;
 chmod -R 777 uploads;
+ 
