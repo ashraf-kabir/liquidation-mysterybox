fd6dc13d2003327dccd7231d5ee2878d5cba4c79
Muhammad <zeeshan72awan@gmail.com>
2021-03-19 19:33:24 +0500

    Video URL

--- mkdcore/custom/Admin/views/InventoryEdit.php
+++ mkdcore/custom/Admin/views/InventoryEdit.php
@@ -238,9 +238,10 @@ if ($layout_clean_mode) {
                 <div class="form-group  col-md-5 col-sm-12">
                     <label for="video_url" style="display: block;">Video URL </label>
 
-                    <?php foreach ($video_url as $key => $video): ?>
+                    <?php if(!empty($video_url)){ 
+                    foreach ($video_url as $key => $video){ ?>
                         <input type="url" style="width: 89%;display: inline;margin: 6px 0px;" class="form-control data-input" id="video_url" name="video_url[]" value="<?php echo $video; ?>"  />
-                    <?php endforeach ?>
+                    <?php } } ?>
                      
                     <button type="button" style="margin-left: 5px;" class="btn btn-primary btn-sm  add_new_video_url">+</button>
                 </div>
