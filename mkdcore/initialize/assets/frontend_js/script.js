b5cf537b281d838f1e05642474de8e7afae68a59
Muhammad <zeeshan72awan@gmail.com>
2021-05-11 04:26:07 +0500

    new update

--- mkdcore/initialize/assets/frontend_js/script.js
+++ mkdcore/initialize/assets/frontend_js/script.js
@@ -872,7 +872,7 @@ $(document).on('click','.add_new_card',function(e){
 
 
 $(document).on('click','.shipping-cost-change', function(){  
-  $(this).parent().parent().find('.selected_item_expected_shipping_date').text($(this).attr('data-expected-date'));  
+  $(this).parent().parent().parent().find('.selected_item_expected_shipping_date').text($(this).attr('data-expected-date'));  
   calculate_cost();
 });
 
