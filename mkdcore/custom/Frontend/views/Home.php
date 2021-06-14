509be6a3b39721f7273b8bbfbfc0fa752753fd5f
Muhammad <zeeshan72awan@gmail.com>
2021-06-14 16:55:08 +0500

    ui updates and fixes

--- mkdcore/custom/Frontend/views/Home.php
+++ mkdcore/custom/Frontend/views/Home.php
@@ -67,7 +67,7 @@
         color: #151414;
         z-index: 2;
         padding: 5px;
-        right: 41%;
+        right: 41%; 
         top: 44%;
     }
 
@@ -97,18 +97,28 @@
 }
 
     }
+
+
+.slider-container{
+    margin-left: 0px !important;
+}
+
+.swiper-container-liquidation .swiper-button-disabled
+{
+    display: none;
+}
 </style>
 
 
-<?php if (!empty($carousel_sliders)): ?>
+<?php if (!empty($carosal_sliders)): ?> 
 
 <div class="swiper-container swiper-container-sliders-custom" style="min-height: 200px;max-height: auto">
     <div class="swiper-wrapper">
-        <?php foreach ($carousel_sliders as $key => $value): ?>
-            <div class="swiper-slide swiper-slide-slider-custom"><img src="<?php echo $value->feature_image ?>"></div>
+        <?php foreach ($carosal_sliders as $key => $value): ?>  
+            <div class="swiper-slide swiper-slide-slider-custom"><img src="<?php echo $value->feature_image ?>"></div> 
         <?php endforeach ?>
     </div>
-
+     
     <div class="swiper-button-next main-swiper-button-next"></div>
     <div class="swiper-button-prev main-swiper-button-prev"></div>
 </div>
@@ -116,48 +126,48 @@
 <?php endif ?>
 
 
-<div class="container">
-    <?php if ($this->session->flashdata('success1')): ?>
+<div class="container"> 
+    <?php if ($this->session->flashdata('success1')): ?> 
         <div class="alert alert-success alert-dismissible " role="alert">
             <?php echo $this->session->flashdata('success1'); ?>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
-        </div>
+        </div> 
     <?php endif ?>
 
-    <?php if ($this->session->flashdata('error1')): ?>
+    <?php if ($this->session->flashdata('error1')): ?> 
         <div class="alert alert-danger alert-dismissible " role="alert">
             <?php echo $this->session->flashdata('error1'); ?>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
-        </div>
+        </div> 
     <?php endif ?>
 </div>
+ 
 
 
 
-
-<?php if (!empty($all_categories)): ?>
+<?php if (!empty($all_categories)): ?> 
 <h5 class="heading-padding-mysterybox pl-4">Shop Mysteryboxes</h5>
-<div class="container slider-container pl-4">
+<div class="container   pl-4"> 
 
     <div class="swiper-container swiper-container-mysterybox mySwiper mySwiper-mysterybox"  style="min-height: 200px;max-height: 300px">
         <div class="swiper-wrapper">
             <?php foreach ($all_categories as $key => $value): ?>
-                <div class="swiper-slide swiper-slide-mysterybox slide-list-box">
+                <div class="swiper-slide swiper-slide-mysterybox slide-list-box"> 
                     <a href="<?php echo base_url() ?>categories/?category=<?php echo $value->id; ?>">
                     <?php if (empty($value->feature_image)): ?>
                         <img class="img-thumbnail" src="./uploads/placeholder.jpg">
                     <?php else: ?>
                         <img class="img-thumbnail"  src="<?php echo $value->feature_image; ?>">
-                    <?php endif ?>
+                    <?php endif ?> 
                     </a>
 
                     <!-- <h2 class="top_div_categories"><?php echo $value->name ?></h2> -->
                 </div>
-            <?php endforeach ?>
+            <?php endforeach ?> 
         </div>
         <div class="swiper-button-next list-swiper-button-next swiper-button-next-mysterybox-card"></div>
         <div class="swiper-button-prev list-swiper-button-prev swiper-button-prev-mysterybox-card"></div>
@@ -167,45 +177,45 @@
 <?php endif ?>
 
 
-
+ 
 <h5 class="heading-padding-liquidation pl-4">Current Inventory</h5>
-<div class="container slider-container pl-4">
+<div class="container   pl-4"> 
 
     <div class="swiper-container swiper-container-liquidation mySwiper mySwiper-liquidation"  style="min-height: 200px;max-height: 300px">
         <div class="swiper-wrapper">
-
+ 
             <div class="swiper-slide swiper-slide-liquidation slide-list-box">
-                <a href="<?php echo $liquidation_url ?>?type=1">
-                    <img class="img-thumbnail"  src="./uploads/shop_pallet.jpg">
+                <a href="<?php echo $liquidation_url ?>?type=1"> 
+                    <img class="img-thumbnail"  src="./uploads/shop_pallet.jpg"> 
                 </a>
             </div>
+            
 
 
 
-
-
+            
             <div class="swiper-slide swiper-slide-liquidation slide-list-box">
-                <a href="<?php echo $liquidation_url ?>?type=2">
-                    <img class="img-thumbnail"  src="./uploads/placeholder.jpg">
-
+                <a href="<?php echo $liquidation_url ?>?type=2"> 
+                    <img class="img-thumbnail"  src="./uploads/placeholder.jpg"> 
+                    
                 </a>
             </div>
+             
 
 
-
-
+ 
             <div class="swiper-slide swiper-slide-liquidation slide-list-box">
-                <a href="<?php echo $liquidation_url ?>?type=3">
-                    <img class="img-thumbnail"  src="./uploads/truckload.jpg">
+                <a href="<?php echo $liquidation_url ?>?type=3"> 
+                    <img class="img-thumbnail"  src="./uploads/truckload.jpg"> 
                 </a>
             </div>
-
+            
         </div>
         <div class="swiper-button-next list-swiper-button-next swiper-button-next-liquidation-card"></div>
         <div class="swiper-button-prev list-swiper-button-prev swiper-button-prev-liquidation-card"></div>
         <div class="swiper-pagination swiper-pagination-liquidation-card"></div>
     </div>
-</div>
+</div> 
 
 
 
