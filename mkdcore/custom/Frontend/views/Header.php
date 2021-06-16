75cfa8b902f1e8e5ed3f20d60ca25bf1ca71cfe5
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2021-06-16 19:20:50 +0545

    few changes

--- mkdcore/custom/Frontend/views/Header.php
+++ mkdcore/custom/Frontend/views/Header.php
@@ -7,7 +7,7 @@
 
      <!-- Bootstrap CSS -->
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sanjaya007/flex-library/sanjaya.min.css" crossorigin="anonymous">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/sanjaya007/flex-library@master/dist/css/sanjaya.min.css" crossorigin="anonymous">
 
      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
