c0fb39a0a525c24478140b7e9342b443912af453
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2021-06-21 22:40:27 +0545

    few changes

--- mkdcore/custom/Frontend/views/Home.php
+++ mkdcore/custom/Frontend/views/Home.php
@@ -79,6 +79,9 @@
     padding: 35px 19px;
     transform: scale(0.8);
 }
+.swiper-button-next:hover{
+    padding: 0 !important;
+}
 
     @media (max-width: 991px){
         .main-swiper-button-next{
