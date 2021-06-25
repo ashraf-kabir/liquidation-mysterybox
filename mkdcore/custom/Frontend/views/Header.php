01e2562053bae24991bc8fcb01db7e4fb62e27e4
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2021-06-25 17:07:13 +0545

    few changes

--- mkdcore/custom/Frontend/views/Header.php
+++ mkdcore/custom/Frontend/views/Header.php
@@ -182,8 +182,8 @@
           .parent-cat .child-cat a:hover{
                background-color: #F6F6F6; 
           }
-          .parent-cat .child-cat a:hover .parent-cat{
-               background-color: red;
+          .parent-cat .child-cat a:hover .parent-cat a{
+               background-color: red !important;
           }
 
           .logo-nav{
