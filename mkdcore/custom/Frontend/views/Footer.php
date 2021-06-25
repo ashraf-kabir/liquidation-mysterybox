26a150b5fb3b95fc9aa8cba7ee99c51d851fa41d
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2021-06-25 18:05:30 +0545

    few changes

--- mkdcore/custom/Frontend/views/Footer.php
+++ mkdcore/custom/Frontend/views/Footer.php
@@ -613,9 +613,13 @@ crossorigin="anonymous"></script>
 
 
   $(".child-cat").on("mouseover", function(e){
-      console.log(e);
       console.log($($(this).parent()).children()[0]);
       $($($(this).parent()).children()[0]).attr("style", "background-color: #E0E0E0 !important;");
+  })
+
+  $(".child-cat").on("mouseout", function(e){
+      console.log($($(this).parent()).children()[0]);
+      $($($(this).parent()).children()[0]).attr("style", "background-color: #ffffff !important;");
   })
 
 
