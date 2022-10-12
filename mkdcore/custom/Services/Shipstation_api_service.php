b7543a7cacee9ad5cc9606ff9e1680730c226f5d
Toheeb Oyekola <Toheeb.Olawale.TO23@gmail.com>
2022-10-12 23:23:19 +0100

    test dump: smarty api response

--- mkdcore/custom/Services/Shipstation_api_service.php
+++ mkdcore/custom/Services/Shipstation_api_service.php
@@ -576,9 +576,7 @@ class Shipstation_api_service
         $resp = json_decode($resp);
         $output = false;
 
-        echo '<pre>';
-        var_dump($resp);
-        echo '</pre>';
+
 
         if (empty($resp)) {
             $output = false;
@@ -586,6 +584,9 @@ class Shipstation_api_service
             $output = isset($resp[0]->metadata->rdi) ?  $resp[0]->metadata->rdi : false;
         }
 
+        echo '<pre>';
+        var_dump($output);
+        echo '</pre>';
         return $output;
     }
 
