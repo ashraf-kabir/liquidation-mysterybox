52605e8ca7ea1fcdc8341f9c7f113eb374a8c5c4
ryanwong <wongryan2001@gmail.com>
2022-11-28 15:17:58 -0500

    old barcode

--- mkdcore/custom/Admin/views/InventoryView.php
+++ mkdcore/custom/Admin/views/InventoryView.php
@@ -361,7 +361,7 @@ if ($layout_clean_mode) {
 
 						<div class='row '>
 							<div class="col-sm-12">
-								<h1 class="make_font_big" ><?php echo $view_model->get_product_name(); ?></h1>
+								<h1 class="make_font_big" style="text-overflow: hidden;clear: both;display: inline-block;overflow: hidden;white-space: nowrap;"><?php echo $view_model->get_product_name(); ?></h1>
 							</div>
 						</div>
 
@@ -408,7 +408,7 @@ if ($layout_clean_mode) {
 
 						<div class='row '>
 							<div class="col-sm-12">
-								<h1 class="make_font_big"> <?php echo $view_model->get_product_name(); ?></h1>
+								<h1 style="text-overflow: hidden;" class="make_font_big"> <?php echo $view_model->get_product_name(); ?></h1>
 							</div>
 						</div>
 
